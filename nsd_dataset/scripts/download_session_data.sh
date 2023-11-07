e09238762f8b63a5cb08ba9595192bf36ebff4ae
Akshett Rai Jindal <akshett.jindal@research.iiit.ac.in>
2023-11-07 15:22:45 +0530

    [nsddata] add script to download brain masks

--- nsd_dataset/scripts/download_session_data.sh
+++ nsd_dataset/scripts/download_session_data.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 for subj in 1 2 5 7; do
+    echo "Downloading for subj 0$subj"
     aws --no-sign-request s3 sync --exclude '*' \
         --include 'func1pt8mm/betas_fithrf_GLMdenoise_RR/betas_session*.*' \
         s3://natural-scenes-dataset/nsddata_betas/ppdata/subj0"$subj" \
+        ./subj0"$subj"
+
+    aws --no-sign-request s3 sync --exclude '*' \
+        --include 'func1pt8mm/brainmask.nii.gz' \
+        s3://natural-scenes-dataset/nsddata/ppdata/subj0"$subj" \
         ./subj0"$subj"
 
     aws --no-sign-request s3 sync --exclude '*' \
