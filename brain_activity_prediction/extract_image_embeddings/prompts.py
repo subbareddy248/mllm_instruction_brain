26236126ee7dc4c3b6a935064dd8f4995513e098
Akshett <jindalakshett@gmail.com>
2024-04-14 14:41:32 +0530

    [fmri_prediction] add image captioning prompts

--- brain_activity_prediction/extract_image_embeddings/prompts.py
+++ brain_activity_prediction/extract_image_embeddings/prompts.py
@@ -1,4 +1,8 @@
 ALL_PROMPTS = {
-    1: "Generate a sentence to describe the content of the image.",
+    # Image Captioning
+    1: "In this task, you will look at the image and briefly describe the image.",
+    2: "What is the caption of the image?",
+    3: "Generate some text to describe the image.",
+    4: "Look at image and tell me what is the content.",
+    5: "In this task, you are given an image and you will need to generate some text to describe it.",
 }
- 
